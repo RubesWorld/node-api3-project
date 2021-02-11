852f75e913cc5d38cbe938503f3f969a36fb2cb0
RubesWorld <hi@rubesworld.live>
2021-02-10 17:40:50 -0800

    Completed middleware creation

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,3 +1,7 @@
+//import models
+const pModels = require("../posts/posts-model");
+const uModels = require("../users/users-model");
+
 const logger = (req, res, next) => {
   console.log(
     `The ${req.method} was made on ${Date().toLocaleString()} to ${
@@ -7,19 +11,55 @@ const logger = (req, res, next) => {
   next();
 };
 
-function validateUserId(req, res, next) {
-  // do your magic!
-}
+const validateUserId = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const user = await uModels.getById(id);
+    if (!user) {
+      res.status(404).json({ message: "user not found" });
+    } else {
+      req.user = user;
+      next();
+    }
+  } catch (err) {
+    res.status(500).json(`server error: ${err}`);
+  }
+};
 
-function validateUser(req, res, next) {
-  // do your magic!
-}
+const validateUser = async (req, res, next) => {
+  if (!req.body.name) {
+    res.status(400).json({ message: "missing required name field" });
+  } else if (!req.body) {
+    res.status(400).json({ message: "missing user data" });
+  } else {
+    next();
+  }
+};
 
 function validatePost(req, res, next) {
-  // do your magic!
+  if (!req.body) {
+    res.status(400).json({ message: "missinge post data" });
+  } else if (!req.body.name) {
+    res.status(400).json({ message: "missing required text field" });
+  } else {
+    next();
+  }
 }
 
-const validatePostId = (req, res, next) => {};
+const validatePostId = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const posts = pModels.getById(id);
+    if (!posts) {
+      res.status(404).json({ message: "user not found" });
+    } else {
+      req.posts = posts;
+      next();
+    }
+  } catch (err) {
+    res.status(500).json(`server error: ${err}`);
+  }
+};
 
 // do not forget to expose these functions to other modules
 
